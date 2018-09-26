5537d7d333629dd95e85a6436c28f6e3b83eb481
Robert Herley <rherley@stevens.edu>
2018-09-26 19:16:35 -0400

    shitty css

--- src/App.js
+++ src/App.js
@@ -1,49 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import firebase from 'firebase';
+import Thread from './components/Thread';
+import _ from 'lodash';
 import 'firebase/firestore';
 import './App.css';
 import { v4 as uuid } from 'uuid';
-import { CreateThread } from './CreateThread';
+import { CreateThread } from './components/CreateThread';
 
 class App extends Component {
-  state = {
-    threads: {}
-  }
+   state = {
+      threads: []
+   };
 
-  createThread(text, image = null) {
-    const id = uuid()
-    firebase.firestore().collection('threads').doc(id).set({
-      id,
-      text,
-      image,
-      createdAt: new Date()
-    })
-  }
+   createThread(text, image = null) {
+      const id = uuid();
+      firebase
+         .firestore()
+         .collection('threads')
+         .doc(id)
+         .set({
+            id,
+            text,
+            image,
+            createdAt: new Date()
+         });
+   }
 
-  componentWillMount() {
-    firebase.firestore().collection('threads').onSnapshot((doc) => {
-      this.setState(state => {
-        const threads = state.threads
-        doc.docChanges().map(({ type, doc }) => {
-          if (type === 'added' || type === 'modified')
-            threads[doc.data().id] = doc.data()
-          else if (type === 'removed')
-            delete threads[doc.data().id]
-        })
-        return { threads }
-      })
-    })
-  }
+   componentWillMount() {
+      firebase
+         .firestore()
+         .collection('threads')
+         .onSnapshot(doc => {
+            this.setState(state => {
+               const threads = state.threads;
+               doc.docChanges().forEach(({ type, doc }) => {
+                  if (type === 'added' || type === 'modified')
+                     threads[doc.data().id] = doc.data();
+                  else if (type === 'removed') delete threads[doc.data().id];
+               });
+               return {
+                  threads: _.sortBy(
+                     Object.values(threads),
+                     v => new Date(v.createdAt)
+                  ).reverse()
+               };
+            });
+         });
+   }
 
-  render() {
-    return (
-      <div className="App">
-        <pre>{JSON.stringify(this.state.threads, null, 2)}</pre>
-        <CreateThread createThread={this.createThread} />
-      </div>
-    );
-  }
+   render() {
+      const { threads } = this.state;
+      return (
+         <div className="App">
+            <CreateThread createThread={this.createThread} />
+            {threads.map(e => (
+               <Thread key={e.id} data={e} />
+            ))}
+         </div>
+      );
+   }
 }
 
 export default App;
