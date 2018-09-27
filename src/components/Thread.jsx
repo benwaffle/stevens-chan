5b1ef2e9f54769a8f94788722ef9629f2a8853dc
Robert Herley <rherley@stevens.edu>
2018-09-26 20:43:52 -0400

    big boi

--- src/components/Thread.jsx
+++ src/components/Thread.jsx
@@ -2,21 +2,35 @@ import React, { Component } from 'react';
 import moment from 'moment';
 
 class Thread extends Component {
+   state = {
+      bigboi: false
+   };
    render() {
       const { data } = this.props;
+      const { bigboi } = this.state;
       return (
          <div className="thread">
             <div className="time">{moment(data.createdAt).fromNow()}</div>
             <div className="split">
-               {data.image && <img src={data.image} alt="garbage" />}
+               {data.image && (
+                  <img
+                     src={data.image}
+                     onClick={() => this.setState({ bigboi: !bigboi })}
+                     style={{
+                        width: !bigboi ? '10em' : '80%',
+                        height: !bigboi ? '10em' : '80%'
+                     }}
+                     alt="garbage"
+                  />
+               )}
                <pre>{data.text}</pre>
             </div>
             <div className="replies">
-              {(data.replies || []).map(reply =>
-                <div key={reply.id} className="reply">
-                  {reply.text}
-                </div>
-              )}
+               {(data.replies || []).map(reply => (
+                  <div key={reply.id} className="reply">
+                     {reply.text}
+                  </div>
+               ))}
             </div>
          </div>
       );
